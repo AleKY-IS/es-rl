discovered linux bug with multiprocessing: softmax etc and conv layers are slow or unresponsive

es/envs.py
@@ -17,7 +17,6 @@ def create_env(env_id, **kwargs):
     spec = gym.spec(env_id)
     return env
 
-
 # Taken from https://github.com/openai/universe-starter-agent
 def create_atari_env(env_id, square_size=42):
     env = gym.make(env_id)
@@ -27,7 +26,6 @@ def create_atari_env(env_id, square_size=42):
     env = NormalizedEnv(env)
     env = Unvectorize(env)
     return env
-
 
 def create_classical_control_env(env_id):
     env = gym.make(env_id)
@@ -47,20 +45,20 @@ class AtariRescale(vectorized.ObservationWrapper):
         self.observation_space = Box(0.0, 1.0, [1, square_size, square_size])
 
     def _observation(self, observation_n):
-        return [_process_frame(observation, square_size=self.square_size) for observation in observation_n]
+        return [self._process_frame(observation) for observation in observation_n]
 
-    def _process_frame(frame, square_size=42):
+    def _process_frame(self, frame):
         frame = frame[34:34 + 160, :160]
         # Resize by half, then down to 42x42 (essentially mipmapping). If
         # we resize directly we lose pixels that, when mapped to 42x42,
         # aren't close enough to the pixel boundary.
-        if square_size < 80:
+        if self.square_size < 80:
             frame = cv2.resize(frame, (80, 80))
-        frame = cv2.resize(frame, (square_size, square_size))
+        frame = cv2.resize(frame, (self.square_size, self.square_size))
         frame = frame.mean(2)
         frame = frame.astype(np.float32)
         frame *= (1.0 / 255.0)
-        frame = np.reshape(frame, [1, square_size, square_size])
+        frame = np.reshape(frame, [1, self.square_size, self.square_size])
         return frame
 
 

es/eval_funs.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn.functional as F
 from torch.autograd import Variable
-
+import time
 import IPython
 
 
@@ -10,24 +10,33 @@ def gym_rollout(args, model, random_seed, return_queue, env, is_antithetic):
     Function to do rollouts of a policy defined by `model` in given environment
     """
     # Reset environment
+    print("Work started")
+    model_time = 0
     state = env.reset()
     state = Variable(torch.from_numpy(state).float(), requires_grad=True).unsqueeze(0)
     retrn = 0
     nsteps = 0
     done = False
     # Rollout
+    total_time = time.clock()
     while not done and nsteps < args.max_episode_length:
         # Choose action
+        model_time_start = time.clock()
         actions = model(state)
+        model_time += time.clock() - model_time_start
         action = actions.max(1)[1].data.numpy()
+        # print("test")
         # Step
         state, reward, done, _ = env.step(action[0])
         retrn += reward
         nsteps += 1
         # Cast state
         state = Variable(torch.from_numpy(state).float(), requires_grad=True).unsqueeze(0)
+        # print("iter " + str(nsteps))
+    total_time = time.clock() - total_time
     #return_queue.put((random_seeds, all_returns, all_num_steps, is_antithetic))
     return_queue.put({'seed': random_seed, 'return': retrn, 'is_anti': is_antithetic, 'nsteps': nsteps})
+    print("Work ended " + str(nsteps) + " tot: " + str(total_time) + " mod: " + str(model_time))
 
 
 def gym_render(args, model, env):
@@ -96,23 +105,25 @@ def gym_rollout_gradients(args, models, random_seeds, return_queue, env, is_anti
     return_queue.put((random_seeds, all_returns, all_num_steps, is_antithetic))
 
 
-def supervised_eval(args, model, random_seed, return_queue, train_data_loader, is_antithetic):
+def supervised_eval(args, model, random_seed, return_queue, train_loader, is_antithetic):
     """
     Function to evaluate the fitness of a supervised model.
 
     For supervised training, the training data set loader is viewed as the "environment"
-    and is passed in the env variable (train_data_loader).
+    and is passed in the env variable (train_loader).
     """
-    print(next(iter(data_loader)))
-    (data, target) = next(iter(data_loader))
-    print(data)
-    print(target)
+    #print(next(iter(train_loader)))
+    (data, target) = next(iter(train_loader))
+    #print(data)
+    #print(target)
     
-    if args.cuda:
-        data, target = data.cuda(), target.cuda()
+    #if args.cuda:
+    #    data, target = data.cuda(), target.cuda()
     data, target = Variable(data), Variable(target)
     output = model(data)
+    print("before")
     loss = -F.nll_loss(output, target)
+    print("after")
 
     assert 2 == 1
     return_queue.put({'seed': random_seed, 'return': loss, 'is_anti': is_antithetic, 'nsteps': nsteps})

es/models.py
@@ -1,6 +1,3 @@
-# Taken from https://github.com/ikostrikov/pytorch-a3c
-from __future__ import absolute_import, division, print_function
-
 import IPython
 import numpy as np
 import torch
@@ -9,7 +6,27 @@ import torch.nn.functional as F
 from torch.autograd import Variable
 
 
+def softmax(x, dim):
+    """
+    Make-shift version of softmax that works on linux.
+    """
+    s = x.data.exp().sum()
+    x.data = x.data.exp()/s
+    return x
+
+def log_softmax(x, dim):
+    x.data = softmax(x, dim).data.log()
+    return x
+
+
 class AbstractESModel(nn.Module):
+    """
+    Abstract models class for models that are trained by evolutionary
+    methods. The models have the .parameters() method, but here an additional
+    .es_parameters() method is added. Normally, parameters are trained/not trained
+    based on the .requires_grad bool. For ES this is not really analogous to being
+    trained or not.
+    """
     def count_parameters(self):
         count = 0
         for param in self.parameters():
@@ -20,7 +37,7 @@ class AbstractESModel(nn.Module):
         """
         The params that should be trained by ES (all of them)
         """
-        return [param for param in self.parameters()]
+        return self.parameters()
 
 
 class FFN(AbstractESModel):
@@ -44,15 +61,10 @@ class FFN(AbstractESModel):
         x = F.relu(self.lin2(x))
         x = F.relu(self.lin3(x))
         x = F.relu(self.lin4(x))
-        x = F.softmax(self.lin5(x), dim=1)
+        x = F.log_softmax(self.lin5(x), dim=1)
+        #x = F.relu(self.lin5(x))
+        #x = log_softmax(self.lin5(x), dim=1)
         return x
-
-    def es_parameters(self):
-        """
-        The params that should be trained by ES (all of them)
-        """
-        return [param for param in self.parameters()]
-
 
 class DQN(AbstractESModel):
     """
@@ -76,13 +88,14 @@ class DQN(AbstractESModel):
         x = x.view(x.size(0), -1)
         x = F.relu(self.lin1(x))
         x = F.softmax(self.lin2(x), dim=1)
+        #x = log_softmax(self.lin2(x), dim=1)
         return x
 
     def _get_conv_output(self, shape):
         """ Compute the number of output parameters from convolutional part by forward pass"""
         bs = 1
-        input = Variable(torch.rand(bs, *shape))
-        output_feat = self._forward_features(input)
+        inputs = Variable(torch.rand(bs, *shape))
+        output_feat = self._forward_features(inputs)
         n_size = output_feat.data.view(bs, -1).size(1)
         return n_size
 
@@ -98,7 +111,7 @@ class MujocoFFN(AbstractESModel):
     The FFN used by Salismans (2017)
     """
     def __init__(self, observation_space, action_space):
-        super(DQN, self).__init__()
+        super(MujocoFFN, self).__init__()
         self.full1 = nn.Linear(observation_space.shape, 2)
 
     def forward(self, x):

es/train.py
@@ -1,17 +1,20 @@
-from __future__ import absolute_import, division, print_function
-
 import math
 import os
+import queue
 import time
 
 import IPython
 import matplotlib.pyplot as plt
 import numpy as np
+
 import torch
 import torch.legacy.optim as legacyOptim
 import torch.multiprocessing as mp
 import torch.nn.functional as F
 from torch.autograd import Variable
+
+from torch import nn
+import gym
 
 
 def perturb_model(args, model, random_seed, env):
@@ -182,13 +185,14 @@ def gradient_update(args, parent_model, returns, random_seeds, is_anti_list):
             np.random.seed(random_seeds[i])
             multiplier = -1 if is_anti_list[i] else 1
             sr = shaped_returns[i]
-            for k, v in parent_model.es_params():
-                eps = np.random.normal(0, 1, v.size())
+            for param in parent_model.es_parameters():
+                eps = np.random.normal(0, 1, param.data.size())
                 grad = torch.from_numpy((args.n*args.sigma) * (sr*multiplier*eps)).float()
-                v += args.lr * grad
+                param.data += args.lr * grad
         args.lr *= args.lr_decay
 
     return parent_model
+
 
 # TODO: Examine possibility of reusing pool of workers 
 #       - p = Pool(args.n)
@@ -209,6 +213,7 @@ def train_loop(args, parent_model, env, eval_fun, chkpt_dir):
         return_queue = mp.Queue()
 
         # Generate a perturbation and its antithesis
+        # TODO: This could be be part of the parallel execution (somehow)
         for j in range(int(args.n/2)):
             random_seed, two_models = generate_seeds_and_models(args, parent_model, env)
             # Add twice because we get two models with the same seed
@@ -236,11 +241,32 @@ def train_loop(args, parent_model, env, eval_fun, chkpt_dir):
         p = mp.Process(target=eval_fun, args=(args, parent_model, 'dummy_seed', return_queue, env, 'dummy_neg'))
         p.start()
         processes.append(p)
-
-        # Get results of started processes
+                
+        # Get output from processes until all are terminated
+        # raw_output = []
+        # while processes:
+        #     # Give tasks a chance to put more data in; more if nothing returned yet
+        #     time.sleep(0.05)
+        #     if return_queue.empty():
+        #         continue
+        #     # Get output from return queue until empty
+        #     try:
+        #         while True:
+        #             raw_output.append(return_queue.get(False))
+        #     except queue.Empty:
+        #         pass
+        #     # Update live processes
+        #     processes = [p for p in processes if p.is_alive()]
+        
+        # Force join
         for p in processes:
             p.join()
+
         workers_end_time = time.clock()
+
+       #  IPython.embed()
+
+        # Get results of finished processes
         raw_output = [return_queue.get() for p in processes]
                 
         # Get all results
@@ -284,7 +310,6 @@ def train_loop(args, parent_model, env, eval_fun, chkpt_dir):
 
         # Plot
         if num_eps % (40*20) == 0:
-            print('Updating plot')
             fig = plt.figure()
             pltUnp, = plt.plot(episodes, reward_unperturbed, label='parent')
             pltAvg, = plt.plot(episodes, reward_average, label='average')
@@ -298,7 +323,6 @@ def train_loop(args, parent_model, env, eval_fun, chkpt_dir):
 
             # Add plot of time used
 
-            print('Updated plot')
             
 
         # Save checkpoint
@@ -322,10 +346,115 @@ def train_loop(args, parent_model, env, eval_fun, chkpt_dir):
                 'Learning rate: %f\n'
                 'Unperturbed reward: %f\n'
                 'Unperturbed rank: %s\n' 
-                'Using Adam: %r\n\n' %
+                'Using Adam: %r' %
                 (num_eps, reward_average[-1], reward_var[-1], reward_max[-1],
                 reward_min[-1], args.n,
                 args.max_episode_length, args.sigma, args.lr,
                 unperturbed_out['return'], rank_diag, args.useAdam))
             print("Worker time: {}".format(workers_end_time-workers_start_time))
             print("Loop time: {}".format(loop_end_time-loop_start_time))
+            print()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class FFN(nn.Module):
+    """
+    FFN for classical control problems
+    """
+
+    def __init__(self, observation_space, action_space):
+        super(FFN, self).__init__()
+        assert hasattr(observation_space, 'shape') and len(
+            observation_space.shape) == 1
+        assert hasattr(action_space, 'n')
+        in_dim = observation_space.shape[0]
+        out_dim = action_space.n
+        self.lin1 = nn.Linear(in_dim, 32)
+        self.lin2 = nn.Linear(32, 64)
+        self.lin3 = nn.Linear(64, 64)
+        self.lin4 = nn.Linear(64, 32)
+        self.lin5 = nn.Linear(32, out_dim)
+
+    def forward(self, x):
+        x = F.relu(self.lin1(x))
+        x = F.relu(self.lin2(x))
+        x = F.relu(self.lin3(x))
+        x = F.relu(self.lin4(x))
+        print("beofre")
+        print(x)
+        x = F.log_softmax(self.lin5(x), dim=1)
+        print("did softmax")
+        # x = F.relu(self.lin5(x))
+        return x
+
+def gym_rollout(max_episode_length, model, random_seed, return_queue, env, is_antithetic):
+    """
+    Function to do rollouts of a policy defined by `model` in given environment
+    """
+    # Reset environment
+    state = env.reset()
+    state = Variable(torch.from_numpy(state).float(),
+                    requires_grad=True).unsqueeze(0)
+    retrn = 0
+    nsteps = 0
+    done = False
+    # Rollout
+    while not done and nsteps < max_episode_length:
+        # Choose action
+        actions = model(state)
+        action = actions.max(1)[1].data.numpy()
+        # Step
+        state, reward, done, _ = env.step(action[0])
+        retrn += reward
+        nsteps += 1
+        # Cast state
+        state = Variable(torch.from_numpy(state).float(),
+                        requires_grad=True).unsqueeze(0)
+    return_queue.put({'seed': random_seed, 'return': retrn,
+                    'is_anti': is_antithetic, 'nsteps': nsteps})
+
+def train_loopasd(args, parent_model, env, eval_fun, chkpt_dir):
+    for _ in range(args.max_gradient_updates):
+        env = gym.make('CartPole-v0')
+        return_queue = mp.Queue()
+
+        models = []
+        for i in range(10):
+            models.append(FFN(env.observation_space, env.action_space))
+
+        processes = []
+        for i in range(10):
+            p = mp.Process(target=gym_rollout, args=(
+                1000, models[i], 'dummy_seed', return_queue, env, 'dummy_neg'))
+            p.start()
+            processes.append(p)
+
+        # Force join
+        for p in processes:
+            p.join()
+
+        # Get results of finished processes
+        raw_output = [return_queue.get() for p in processes]
+        
+
+
+
+
+

rl-experiments/gym_main.py
@@ -39,20 +39,14 @@ if __name__ == '__main__':
     assert args.n % 2 == 0
 
     # Create environment
-    if args.env_name == 'CartPole-v0' or 'CartPole-v1':
+    if args.env_name == 'CartPole-v0' or args.env_name == 'CartPole-v1':
         env = gym.make(args.env_name)
     else:
         env = create_atari_env(args.env_name, square_size=args.frame_size)
-    
+        
     # Create model
     model_class = getattr(es.models, args.model_name)
     parent_model = model_class(env.observation_space, env.action_space)
-    state = env.reset()
-    state = torch.from_numpy(state).float()     # Cast to torch array
-    state = Variable(state, volatile=True)      # Wrap with Variable to become part of graph
-    state = state.unsqueeze(0)                  # Expand dimensions to include batch size of 1
-    actions = parent_model(state)
-    p, a = actions.max(1)
 
     # Remove gradient requirement from parameters
     # for param in parent_model.parameters():
@@ -74,6 +68,9 @@ if __name__ == '__main__':
 
     # Run test or train
     if args.test:
-        render_env(args, parent_model, env)
+        gym_render(args, parent_model, env)
     else:
-        train_loop(args, parent_model, env, gym_rollout, chkpt_dir)
+        try:
+            train_loop(args, parent_model, env, gym_rollout, chkpt_dir)
+        except KeyboardInterrupt:
+            print("Program stopped by keyboard interruption")