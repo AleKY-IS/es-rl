768750936e7b15666ea85db453e16a952bf62960
Jakob D. Havtorn <jakob.havtorn@gmail.com>
2018-01-18 13:52:59 +0100

    Added load_checkpoint method.
    Also changed plot smoothing and added walltimes per generation

--- es/utils.py
+++ es/utils.py
@@ -1,12 +1,15 @@
 import inspect
+import os
+import pickle
+import pprint
 
 import IPython
 import matplotlib.pyplot as plt
-import torch
-import pprint
-import os
-import pickle
+import numpy as np
 import pandas as pd
+
+import torch
+
 
 def print_init(args, model, optimizer, lr_scheduler):
     print("================= Evolver ==================")
@@ -29,7 +32,7 @@ def print_init(args, model, optimizer, lr_scheduler):
 def print_iter(args, stats, workers_start_time, workers_end_time, loop_start_time):
     print()
     try:
-        s = "Gen {:3d} | Obs {:9d} | F {:5.2f} | Avg {:5.2f} | Max {:5.2f} | Min {:5.2f} | Var {:5.2f} | Rank {:3d} | Sig {:5.2f} | LR {:5.2f}".format(
+        s = "Gen {:5d} | Obs {:9d} | F {:6.2f} | Avg {:6.2f} | Max {:6.2f} | Min {:6.2f} | Var {:7.2f} | Rank {:3d} | Sig {:5.4f} | LR {:5.4f}".format(
         stats['generations'][-1], stats['observations'][-1], stats['reward_unp'][-1], stats['reward_avg'][-1], stats['reward_max'][-1], stats['reward_min'][-1], stats['reward_var'][-1], stats['unp_rank'][-1], stats['sigma'][-1], stats['lr'][-1])
         print(s, end="")
     except Exception:
@@ -65,6 +68,30 @@ def get_lr(optimizer):
     return lr
 
 
+def load_checkpoint(restore_dir, file_path, model, optimizer, lr_scheduler, load_best=False):
+    """
+    Loads a checkpoint saved in the directory `restore_dir` which is a subfolder of the `file_path` of the
+    calling function.
+    """
+    chkpt_dir = file_path+'/'+'/'.join([i for i in restore_dir.split('/') if i not in file_path.split('/')])
+    try:
+        if load_best:
+            model_state_dict = torch.load(os.path.join(chkpt_dir, 'best_model_state_dict.pth'))
+            optimizer_state_dict = torch.load(os.path.join(chkpt_dir, 'best_optimizer_state_dict.pth'))
+        else:
+            model_state_dict = torch.load(os.path.join(chkpt_dir, 'model_state_dict.pth'))
+            optimizer_state_dict = torch.load(os.path.join(chkpt_dir, 'optimizer_state_dict.pth'))
+        with open(os.path.join(chkpt_dir, 'stats.pkl'), 'rb') as filename:
+            stats = pickle.load(filename)
+    except Exception:
+        print("Checkpoint restore failed")
+        raise Exception
+    lr_scheduler.last_epoch = stats['generations'][-1]
+    model.load_state_dict(model_state_dict)
+    optimizer.load_state_dict(optimizer_state_dict)
+    return chkpt_dir, model, optimizer, lr_scheduler, stats
+
+
 def save_checkpoint(parent_model, optimizer, best_model_stdct, best_optimizer_stdct, stats, chkpt_dir):
     # Save latest model and optimizer state
     torch.save(parent_model.state_dict(), os.path.join(chkpt_dir, 'model_state_dict.pth'))
@@ -79,42 +106,79 @@ def save_checkpoint(parent_model, optimizer, best_model_stdct, best_optimizer_st
         pickle.dump(stats, filename, pickle.HIGHEST_PROTOCOL)
 
 
-def smooth(y, factor=10):
+def moving_average(y, window=20, center=True):
     if type(y) != list:
         y = list(y)
-    return pd.Series(y).rolling(window=factor).mean()
+    return pd.Series(y).rolling(window=window, center=center).mean()
 
 
 def plot_stats(stats, chkpt_dir):
-    # NOTE: Possible x-axis are: generations, episodes, observations, time
-    fig = plt.figure()
-    pltUnp, = plt.plot(stats['generations'], stats['reward_unp'], label='parent')
-    pltAvg, = plt.plot(stats['generations'], stats['reward_avg'], label='average')
-    pltMax, = plt.plot(stats['generations'], stats['reward_max'], label='max')
-    pltMin, = plt.plot(stats['generations'], stats['reward_min'], label='min')
-    plt.ylabel('rewards')
-    plt.xlabel('generations')
-    plt.legend(handles=[pltUnp, pltAvg, pltMax, pltMin])
-    fig.savefig(chkpt_dir+'/rew_gen.pdf')
-    plt.close(fig)
+    """
+    Plots training statistics
+    - Unperturbed return
+    - Average return
+    - Maximum return
+    - Minimum return
+    - Smoothed version of the above
+    - Return variance
+    - Rank of unperturbed model
+    - Sigma
+    - Learning rate
+    - Total wall clock time
+    - Wall clock time per generation
+    """
 
-    fig = plt.figure()
-    pltUnp, = plt.plot(stats['generations'], stats['reward_unp'], label='parent')
-    plt.ylabel('rewards')
-    plt.xlabel('generations')
-    plt.legend(handles=[pltUnp])
-    fig.savefig(chkpt_dir+'/rew_gen_parent.pdf')
-    plt.close(fig)
+    plt.rc('font', family='sans-serif')
+    plt.rc('xtick', labelsize='x-small')
+    plt.rc('ytick', labelsize='x-small')
+    figsize = (4, 3)
+    # NOTE: Possible x-axis are: generations, episodes, observations, walltimes
+    for x in ['generations', 'observations', 'walltimes']:
+        fig = plt.figure(figsize=figsize)
+        pltUnp, = plt.plot(stats[x], stats['reward_unp'], label='parent')
+        pltAvg, = plt.plot(stats[x], stats['reward_avg'], label='average')
+        pltMax, = plt.plot(stats[x], stats['reward_max'], label='max')
+        pltMin, = plt.plot(stats[x], stats['reward_min'], label='min')
+        plt.ylabel('rewards')
+        plt.xlabel(x)
+        plt.legend(handles=[pltUnp, pltAvg, pltMax, pltMin])
+        fig.savefig(chkpt_dir+'/rew_' + x[0:3] + '.pdf')
+        plt.close(fig)
 
-    fig = plt.figure()
-    pltUnpS, = plt.plot(stats['generations'], smooth(stats['reward_unp']), alpha=1, label='smoothed parent')
-    plt.gca().set_prop_cycle(None)
-    pltUnp, = plt.plot(stats['generations'], stats['reward_unp'], alpha=.3, label='raw parent')
-    plt.ylabel('rewards')
-    plt.xlabel('generations')
-    plt.legend(handles=[pltUnpS, pltUnp])
-    fig.savefig(chkpt_dir+'/rew_gen_parent_smooth.pdf')
-    plt.close(fig)
+        fig = plt.figure()
+        pltUnp, = plt.plot(stats[x], stats['reward_unp'], label='parent')
+        plt.ylabel('rewards')
+        plt.xlabel(x)
+        plt.legend(handles=[pltUnp])
+        fig.savefig(chkpt_dir+'/rewpar_' + x[0:3] + '.pdf')
+        plt.close(fig)
+
+        fig = plt.figure()
+        pltUnp, = plt.plot(stats[x], moving_average(stats['reward_unp']), label='smoothed parent')
+        pltAvg, = plt.plot(stats[x], moving_average(stats['reward_avg']), label='smoothed average')
+        pltMax, = plt.plot(stats[x], moving_average(stats['reward_max']), label='smoothed max')
+        pltMin, = plt.plot(stats[x], moving_average(stats['reward_min']), label='smoothed min')
+        plt.gca().set_prop_cycle(None)
+        pltUnpBack, = plt.plot(stats[x], stats['reward_unp'], alpha=0.3, label='parent')
+        pltAvgBack, = plt.plot(stats[x], stats['reward_avg'], alpha=0.3, label='average')
+        pltMaxBack, = plt.plot(stats[x], stats['reward_max'], alpha=0.3, label='max')
+        pltMinBack, = plt.plot(stats[x], stats['reward_min'], alpha=0.3, label='min')
+        plt.ylabel('rewards')
+        plt.xlabel(x)
+        plt.legend(handles=[pltUnp, pltAvg, pltMax, pltMin, pltUnpBack, pltAvgBack, pltMaxBack, pltMinBack])
+        fig.savefig(chkpt_dir+'/rewsmo_' + x[0:3] + '.pdf')
+        plt.close(fig)
+
+        #IPython.embed()
+        fig = plt.figure()
+        pltUnpS, = plt.plot(stats[x], moving_average(stats['reward_unp']), alpha=1, label='smoothed parent')
+        plt.gca().set_prop_cycle(None)
+        pltUnp, = plt.plot(stats[x], stats['reward_unp'], alpha=.3, label='raw parent')
+        plt.ylabel('rewards')
+        plt.xlabel(x)
+        plt.legend(handles=[pltUnpS, pltUnp])
+        fig.savefig(chkpt_dir+'/rewparsmo_' + x[0:3] + '.pdf')
+        plt.close(fig)
 
     fig = plt.figure()
     pltVar, = plt.plot(stats['generations'], stats['reward_var'], label='reward variance')
@@ -150,8 +214,16 @@ def plot_stats(stats, chkpt_dir):
 
     fig = plt.figure()
     pltVar, = plt.plot(stats['generations'], stats['walltimes'], label='lr')
-    plt.ylabel('walltimes')
+    plt.ylabel('walltime')
     plt.xlabel('generations')
     plt.legend(handles=[pltVar])
     fig.savefig(chkpt_dir+'/time_gen.pdf')
+    plt.close(fig)
+
+    fig = plt.figure()
+    pltVar, = plt.plot(stats['generations'][:-1], np.diff(stats['walltimes']), label='lr')
+    plt.ylabel('walltime per generation')
+    plt.xlabel('generations')
+    plt.legend(handles=[pltVar])
+    fig.savefig(chkpt_dir+'/timeper_gen.pdf')
     plt.close(fig)
