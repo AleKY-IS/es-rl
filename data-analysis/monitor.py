0f14c3a6dd88aef7b53290ee506aee793cf17f85
JakobHavtorn <jakob.havtorn@gmail.com>
2018-01-23 21:39:42 +0100

    Add count down method

--- data-analysis/monitor.py
+++ data-analysis/monitor.py
@@ -1,6 +1,7 @@
 import argparse
 import os
 import pickle
+import sys
 import time
 
 import IPython
@@ -33,28 +34,57 @@ def invert_signs(sl):
     return sl
 
 
+def count_down(wait=60):
+    for i in range(wait):
+        print("Updating in {:s} seconds".format(str(wait-i)), end="\r")
+        time.sleep(1)
+    print("Updating plots and statistics...", end="\r")
+
+
+def plot_mas(args, sl):
+    fig = plt.figure()
+    handles = []
+    maxs = []
+    for s in sl:
+        ydata = moving_average(s['return_unp'])
+        maxs.append(np.max(ydata))
+        handles.extend(plt.plot(s['generations'], ydata, label="asd"))
+    plt.ylabel('Return')
+    plt.xlabel('Generations')
+    plt.legend(handles=handles)
+    plt.gca().set_ylim([0, np.median(maxs)*1.2])
+    fig.savefig(os.path.join(args.dir, 'monitoring' + '.pdf'))
+    plt.close(fig)
+
+
+def print_stats(args, sl):
+    maxs = []
+    mins = []
+    for s in sl:
+        maxs.append(np.max(s['return_unp']))
+        mins.append(np.min(s['return_unp']))
+    print("=============== Statistics ===============")
+    print("Max return {:>7.3f}".format(np.max(maxs)))
+    print("Min return {:>7.3f}".format(np.min(mins)))
+    print("==========================================")
+
+
 def monitor(args):
+    plt.ion()
     # Get list of subdirectories (checkpoints)
     checkpoint_directories = [os.path.join(args.dir, di) for di in os.listdir(args.dir) if os.path.isdir(os.path.join(args.dir, di))]
 
     # Monitoring loop
     while True:
-        # IPython.embed()
-
+        # Load data
         sl = load_statistics(checkpoint_directories)
         sl = invert_signs(sl)
-
-        fig = plt.figure()
-        handles = []
-        for s in sl:
-            handles.extend(plt.plot(s['generations'], moving_average(s['return_unp']), label="asd"))
-        plt.ylabel('Return')
-        plt.xlabel('Generations')
-        plt.legend(handles=handles)
-        fig.savefig(os.path.join(args.dir, 'monitoring' + '.pdf'))
-        plt.close(fig)
-
-        time.sleep(5)
+        # Print 
+        print_stats(args, sl)
+        # Plot
+        plot_mas(args, sl)
+        # Pause and count down until next update
+        count_down(5)
 
 
 if __name__ == '__main__':
@@ -66,6 +96,6 @@ if __name__ == '__main__':
     try:
         monitor(args)
     except KeyboardInterrupt:
-        print("Monitoring halted by user KeyboardInterrupt")
+        print("\nMonitoring halted by user KeyboardInterrupt")
 
 # python monitor.py --dir /Users/Jakob/mnt/Documents/es-rl/supervised-experiments/checkpoints/
