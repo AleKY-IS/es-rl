fdcc4ec6f55038e8ae03a94b2499a8a70faba6a1
Jakob D. Havtorn <jakob.havtorn@gmail.com>
2018-01-28 16:50:08 +0100

    Added help information to script

--- hpc/submit_batch_hpc.sh
+++ hpc/submit_batch_hpc.sh
@@ -13,28 +13,38 @@ POSITIONAL=()
 while [[ $# -gt 0 ]]
 do
 	key="$1"
-
 	case $key in
-	    -i|--identification)
+	    -n|--name)
 		    TEST_ID="$2"
 		    shift # past argument
 		    shift # past value
 		    ;;
 	    -q|--queue)
 		    QUEUE="$2"
-		    shift # past argument
-		    shift # past value
+		    shift
+		    shift
 		    ;;
 	    -c|--cores)
 		    CORES="$2"
-		    shift # past argument
-		    shift # past value
+		    shift
+		    shift
 		    ;;
 	    -t|--timelimit)
 		    TIME_LIMIT="$2"
-		    shift # past argument
-		    shift # past value
+		    shift
+		    shift
 		    ;;
+		-h|--help)
+			echo "Help for submit_batch_hpc.sh script"
+			echo "This script submits a batch of jobs to the HPC cluster."
+			echo "Options:"
+			echo "	-h, --help       Display help"
+			echo "	-n, --name       Name of the job"
+			echo "	-q, --queue      The HPC queue to submit to"
+			echo "	-c, --cores      The number of cores to use for execution"
+			echo "	-t, --timelimit  The wall clock time limit of the job after which it is terminated"
+			exit # end script if help displayed
+			;;
 	    *)    # unknown option
 		    POSITIONAL+=($key) # save it in an array for later
 		    shift # past argument
@@ -50,12 +60,7 @@ echo $POSITIONAL
 FOO=${TEST_ID:="default-name"}
 FOO=${QUEUE:="hpc"}
 FOO=${CORES:="20"}
-FOO=${TIME_LIMIT:="10:00"}
-
-for i in {0..$REPEATS..2}
-  do 
-     echo "Welcome $i times"
- done
+FOO=${TIME_LIMIT:="15:00"}
 
 # For each input string, submit the job using bsub
 for i in "${!INPUTS[@]}"
@@ -65,16 +70,11 @@ do
 		echo ""
 		NAME="$TEST_ID-$i-$j"
 		bsub -q $QUEUE -J $NAME -W $TIME_LIMIT -n $CORES -R "span[hosts=1] rusage[mem=6GB]" -o "$NAME.log" "sh $SCRIPT ${INPUTS[i]}"
+		# source activate ml
+		# python ../experiments/main.py ${INPUTS[i]}
 		echo "It had inputs: ${INPUTS[i]}"
 		echo "The bash call was:"
 		echo "$ bsub -q $QUEUE -J $NAME -W $TIME_LIMIT -n $CORES -R "span[hosts=1] rusage[mem=6GB]" -o "$NAME.log" "sh $SCRIPT ${INPUTS[i]}""
-
-		# bash run_arg_hpc.sh ${INPUTS[i]}
-		# bsub < run_arg_hpc.sh
 	done
 done
 echo ""
-
-
-sleep 5
-watch qstat $QUEUE
