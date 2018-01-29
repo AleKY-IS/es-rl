778d74d95add9afe07d6afceea6387e63cc3a542
Jakob D. Havtorn <jakob.havtorn@gmail.com>
2018-01-29 11:13:58 +0100

    Cleaned

--- es/models.py
+++ es/models.py
@@ -153,14 +153,11 @@ class MNISTNet(AbstractESModel):
 
 class CapsuleLayer(nn.Module):
     def __init__(self, num_capsules, num_route_nodes, in_channels, out_channels, kernel_size=None, stride=None,
-                 num_iterations=NUM_ROUTING_ITERATIONS):
+                 num_iterations=3):
         super(CapsuleLayer, self).__init__()
-
         self.num_route_nodes = num_route_nodes
         self.num_iterations = num_iterations
-
         self.num_capsules = num_capsules
-
         if num_route_nodes != -1:
             self.route_weights = nn.Parameter(torch.randn(num_capsules, num_route_nodes, in_channels, out_channels))
         else:
@@ -176,12 +173,10 @@ class CapsuleLayer(nn.Module):
     def forward(self, x):
         if self.num_route_nodes != -1:
             priors = x[None, :, :, None, :] @ self.route_weights[:, None, :, :, :]
-
             logits = Variable(torch.zeros(*priors.size())).cuda()
             for i in range(self.num_iterations):
                 probs = capsule_softmax(logits, dim=2)
                 outputs = self.squash((probs * priors).sum(dim=2, keepdim=True))
-
                 if i != self.num_iterations - 1:
                     delta_logits = (priors * outputs).sum(dim=-1, keepdim=True)
                     logits = logits + delta_logits
@@ -189,20 +184,17 @@ class CapsuleLayer(nn.Module):
             outputs = [capsule(x).view(x.size(0), -1, 1) for capsule in self.capsules]
             outputs = torch.cat(outputs, dim=-1)
             outputs = self.squash(outputs)
-
         return outputs
 
 
 class CapsuleNet(nn.Module):
     def __init__(self, n_classes):
         super(CapsuleNet, self).__init__()
-
         self.conv1 = nn.Conv2d(in_channels=1, out_channels=256, kernel_size=9, stride=1)
         self.primary_capsules = CapsuleLayer(num_capsules=8, num_route_nodes=-1, in_channels=256, out_channels=32,
                                              kernel_size=9, stride=2)
         self.digit_capsules = CapsuleLayer(num_capsules=n_classes, num_route_nodes=32 * 6 * 6, in_channels=8,
                                            out_channels=16)
-
         self.decoder = nn.Sequential(
             nn.Linear(16 * n_classes, 512),
             nn.ReLU(inplace=True),
@@ -216,17 +208,13 @@ class CapsuleNet(nn.Module):
         x = F.relu(self.conv1(x), inplace=True)
         x = self.primary_capsules(x)
         x = self.digit_capsules(x).squeeze().transpose(0, 1)
-
         classes = (x ** 2).sum(dim=-1) ** 0.5
         classes = F.softmax(classes)
-
         if y is None:
             # In all batches, get the most active capsule.
             _, max_length_indices = classes.max(dim=1)
             y = Variable(torch.sparse.torch.eye(n_classes)).cuda().index_select(dim=0, index=max_length_indices.data)
-
         reconstructions = self.decoder((x * y[:, :, None]).view(x.size(0), -1))
-
         return classes, reconstructions
 
 
@@ -238,10 +226,7 @@ class CapsuleLoss(nn.Module):
     def forward(self, images, labels, classes, reconstructions):
         left = F.relu(0.9 - classes, inplace=True) ** 2
         right = F.relu(classes - 0.1, inplace=True) ** 2
-
         margin_loss = labels * left + 0.5 * (1. - labels) * right
         margin_loss = margin_loss.sum()
-
         reconstruction_loss = self.reconstruction_loss(reconstructions, images)
-
         return (margin_loss + 0.0005 * reconstruction_loss) / images.size(0)
