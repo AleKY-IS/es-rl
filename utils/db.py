7ca05bfe8b747152ec7085fd3cfbef187bf07d18
JakobHavtorn <jakob.havtorn@gmail.com>
2018-02-09 11:04:19 +0100

    Require token file path. Handle InternalServerError exception.

--- utils/db.py
+++ utils/db.py
@@ -1,12 +1,15 @@
 import os
+import time
 
 import dropbox
-from dropbox.exceptions import ApiError
+from dropbox.exceptions import ApiError, InternalServerError
 from dropbox.files import WriteMode
 
+import random
 
-def get_dropbox_client():
-    with open('dropboxtoken.tok') as f:
+
+def get_dropbox_client(token_file):
+    with open(token_file) as f:
         token = f.readline()[:-1]
     return dropbox.Dropbox(token)
 
@@ -29,16 +32,24 @@ def copy_dir(dbx, source_dir, target_dir, mode='overwrite', silent=False):
             # Put file
             with open(local_path, 'rb') as f:
                 try:
+                    print("    Uploading: " + relative_path, end='\r')
                     dbx.files_upload(f.read(), dropbox_path, mode=WriteMode('overwrite'))
-                    print("    Uploaded " + relative_path)
                 except ApiError as err:
                     # This checks for the specific error where a user doesn't have
                     # enough Dropbox space quota to upload this file
                     if err.error.is_path() and err.error.get_path().reason.is_insufficient_space():
-                        print("    Cannot copy " + relative_path + ". Insufficient space.")
+                        print("    Failed to copy " + relative_path + ". Insufficient space.")
                     if err.error.is_path() and err.error.get_path().reason.is_conflict():
-                        print("    Cannot copy " + relative_path + ". Write conflict.")
+                        print("    Failed to copy " + relative_path + ". Write conflict.")
                     elif err.user_message_text:
                         print(err.user_message_text)
                     else:
                         print(err)
+                except InternalServerError as err:
+                    print("    Failed to upload " + relative_path + ". Internal server error. Trying again. ", end='')
+                    time.sleep(1)
+                    try:
+                        dbx.files_upload(f.read(), dropbox_path, mode=WriteMode('overwrite'))
+                        print("Success.")
+                    except:
+                        print("Failed.")
