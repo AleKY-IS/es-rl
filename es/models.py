ccb4d2ce6cc183d2aae337136865f8def05412d1
Jakob Havtorn <jakob.havtorn@gmail.com>
2018-03-21 16:34:48 +0100

    Better action space handling and use of batch norm layers

--- es/models.py
+++ es/models.py
@@ -1,12 +1,17 @@
+from functools import partial
 from itertools import chain, islice, tee
 
+import gym
 import IPython
 import numpy as np
-
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.autograd import Variable
+from torch.nn.modules.module import _addindent
+
+from context import utils
+from utils.torchutils import torch_summarize
 
 
 def model_weight_initializer(model):
@@ -55,63 +60,150 @@ def capsule_softmax(input, dim=1):
     return softmaxed_output.view(*transposed_input.size()).transpose(dim, len(input.size()) - 1)
 
 
+class IdentityFunction(nn.Module):
+    """Identity activation function module for use when no activation is needed but a function call is.
+    """
+    def __init__(self):
+        super(IdentityFunction, self).__init__()
+
+    def forward(self, x):
+        return x
+
+
 class AbstractESModel(nn.Module):
+    """Abstract models class for models that are trained by evolutionary methods.
+    It has methods for counting parameters, layers and tensors.
     """
-    Abstract models class for models that are trained by evolutionary
-    methods. The models have the .parameters() method, but here an additional
-    .es_parameters() method is added. Normally, parameters are trained/not trained
-    based on the .requires_grad bool. For ES this is not really analogous to being
-    trained or not.
+    @property
+    def summary(self):
+        if not hasattr(self, '_summary'):
+            self._summary = torch_summarize(self, self.in_dim, weights=True, input_shape=True, nb_trainable=True)
+        return self._summary
+
+    def count_parameters(self, only_trainable=True):
+        """Return the number of [trainable] parameters in this model.
+        """
+        return self._count_parameters(self, only_trainable=only_trainable)
+
+    @staticmethod
+    def _count_parameters(m, only_trainable=True):
+        """Count the number of [trainable] parameters in a pytorch model.
+        """
+        k = 'nb_trainable' if only_trainable else 'nb_params'
+        return int(m.summary[k].sum())
+
+    def count_tensors(self, only_trainable=True):
+        return self._count_tensors(self, only_trainable=only_trainable)
+
+    @staticmethod
+    def _count_tensors(m, only_trainable=True):
+        """Count the number of [trainable] tensors in a pytorch model
+        """
+        k = 'nb_trainable' if only_trainable else 'nb_params'
+        return sum([1 for i, l in m.summary.iterrows() for w in l['weights'] if l['weights'] and l[k] > 0])
+
+    def count_layers(self, only_trainable=True):
+        """Count the number of [trainable] layers in a pytorch model.
+        A layer is defined as a module with a nonzero number of [trainable] parameters.
+        """
+        return self._count_layers(self, only_trainable=only_trainable)
+    
+    @staticmethod
+    def _count_layers(m, only_trainable=True):
+        k = 'nb_trainable' if only_trainable else 'nb_params'
+        return m.summary[m.summary[k] > 0].shape[0]
+
+
+def transform_range(in_value, in_maxs, in_mins, out_maxs, out_mins):
+    """Transform a number from a range into another range, maintaining ratios.
     """
-    def count_parameters(self):
-        count = 0
-        for param in self.parameters():
-            count += param.data.numpy().flatten().shape[0]
-        return count
-
-    def es_parameters(self):
-        """
-        The params that should be trained by ES (all of them)
-        """
-        return self.parameters()
+    assert (in_value <= in_maxs).all() and (in_value >= in_mins).all()
+    in_range = (in_maxs - in_mins)  
+    out_range = (out_maxs - out_mins)  
+    return (((in_value - in_mins) * out_range) / in_range) + out_mins
 
 
-class FFN(AbstractESModel):
+def apply_sigmoid_and_transform(x, **kwargs):
+    """Applies the element wise sigmoid function and transforms output into a given range
+    """
+    view_dim = kwargs.pop('view_dim')
+    a = nn.Sigmoid()
+    y = a(x)
+    # y.data = torch.from_numpy(transform_range(y.data.numpy(), **kwargs))
+    y = y.view(view_dim)
+    return y
+
+
+class ClassicalControlFFN(AbstractESModel):
     """
     FFN for classical control problems
     """
     def __init__(self, observation_space, action_space):
-        super(FFN, self).__init__()
+        super(ClassicalControlFFN, self).__init__()
+        if type(action_space) is gym.spaces.Box:
+            # Continuous action space:
+            # Physical output to be used directly.
+            self.out_dim = action_space.shape
+            self.n_out = int(np.prod(action_space.shape))
+            out_mins = action_space.low if not np.isinf(action_space.low).any() else - np.ones(action_space.shape)
+            out_maxs = action_space.high if not np.isinf(action_space.high).any() else np.ones(action_space.shape)
+            sigmoid_mins = - np.ones(out_mins.shape)
+            sigmoid_maxs = np.ones(out_maxs.shape)
+            trsf_in = {'view_dim': (-1, *self.out_dim), 'in_maxs': sigmoid_maxs, 'in_mins': sigmoid_mins, 'out_maxs': out_maxs, 'out_mins': out_mins}
+            self.out_activation = partial(apply_sigmoid_and_transform, **trsf_in)
+        elif type(action_space) is gym.spaces.Discrete:
+            # Discrete action space: 
+            # Probabilistic output to be indexed by maximum probability.
+            # Output is the index of the most likely action.
+            self.n_out = action_space.n
+            self.out_activation = nn.LogSoftmax(dim=1)
+        elif type(action_space) is gym.spaces.MultiDiscrete:
+            IPython.embed()
+            pass
+        elif type(action_space) is gym.spaces.MultiBinary:
+            IPython.embed()
+            pass
+        elif type(action_space) is gym.spaces.tuple:
+            # Tuple of different action spaces
+            # https://github.com/openai/gym/blob/master/gym/envs/algorithmic/algorithmic_env.py
+            IPython.embed()
+            pass
+
         assert hasattr(observation_space, 'shape') and len(observation_space.shape) == 1
-        assert hasattr(action_space, 'n')
-        in_dim = observation_space.shape[0]
-        out_dim = action_space.n
-        self.lin1 = nn.Linear(in_dim, 32)
+        assert hasattr(action_space, 'shape')
+        self.in_dim = observation_space.shape
+        self.n_in = int(np.prod(observation_space.shape))
+        self.lin1 = nn.Linear(self.n_in, 32)
+        self.relu1 = nn.ReLU()
         self.lin2 = nn.Linear(32, 64)
+        self.relu2 = nn.ReLU()
         self.lin3 = nn.Linear(64, 64)
+        self.relu3 = nn.ReLU()
         self.lin4 = nn.Linear(64, 32)
-        self.lin5 = nn.Linear(32, out_dim)
+        self.relu4 = nn.ReLU()
+        self.lin5 = nn.Linear(32, self.n_out)
 
     def forward(self, x):
-        x = F.relu(self.lin1(x))
-        x = F.relu(self.lin2(x))
-        x = F.relu(self.lin3(x))
-        x = F.relu(self.lin4(x))
-        x = F.log_softmax(self.lin5(x), dim=1)
-        #x = F.relu(self.lin5(x))
-        #x = log_softmax(self.lin5(x), dim=1)
+        x = self.relu1(self.lin1(x))
+        x = self.relu2(self.lin2(x))
+        x = self.relu3(self.lin3(x))
+        x = self.relu4(self.lin4(x))
+        x = self.out_activation(self.lin5(x))
         return x
+
 
 class DQN(AbstractESModel):
     """
-    The CNN used by Mnih et al (2015)
+    The CNN used by Mnih et al (2015) for Atari environments
     """
     def __init__(self, observation_space, action_space):
         super(DQN, self).__init__()
         assert hasattr(observation_space, 'shape') and len(observation_space.shape) == 3
         assert hasattr(action_space, 'n')
+        self.in_dim = observation_space.shape
         in_channels = observation_space.shape[0]
         out_dim = action_space.n
+        IPython.embed()
         self.conv1 = nn.Conv2d(in_channels, out_channels=32, kernel_size=(8, 8), stride=(4, 4))
         self.conv2 = nn.Conv2d(32, out_channels=64, kernel_size=(4, 4), stride=(2, 2))
         self.conv3 = nn.Conv2d(64, out_channels=64, kernel_size=(3, 3), stride=(1, 1))
@@ -124,7 +216,6 @@ class DQN(AbstractESModel):
         x = x.view(x.size(0), -1)
         x = F.relu(self.lin1(x))
         x = F.softmax(self.lin2(x), dim=1)
-        #x = log_softmax(self.lin2(x), dim=1)
         return x
 
     def _get_conv_output(self, shape):
@@ -144,16 +235,66 @@ class DQN(AbstractESModel):
 
 class MujocoFFN(AbstractESModel):
     """
-    The FFN used by Salismans (2017)
+    FFN for Mujoco control problems
     """
     def __init__(self, observation_space, action_space):
         super(MujocoFFN, self).__init__()
-        self.full1 = nn.Linear(observation_space.shape, 2)
+        if type(action_space) is gym.spaces.Box:
+            # Continuous action space:
+            # Physical output to be used directly.
+            self.out_dim = action_space.shape
+            self.n_out = int(np.prod(action_space.shape))
+            out_mins = action_space.low if not np.isinf(action_space.low).any() else - np.ones(action_space.shape)
+            out_maxs = action_space.high if not np.isinf(action_space.high).any() else np.ones(action_space.shape)
+            sigmoid_mins = - np.ones(out_mins.shape)
+            sigmoid_maxs = np.ones(out_maxs.shape)
+            trsf_in = {'view_dim': (-1, *self.out_dim), 'in_maxs': sigmoid_maxs, 'in_mins': sigmoid_mins, 'out_maxs': out_maxs, 'out_mins': out_mins}
+            self.out_activation = partial(apply_sigmoid_and_transform, **trsf_in)
+        elif type(action_space) is gym.spaces.Discrete:
+            # Discrete action space: 
+            # Probabilistic output to be indexed by maximum probability.
+            # Output is the index of the most likely action.
+            self.n_out = action_space.n
+            self.out_activation = nn.LogSoftmax(dim=1)
+        elif type(action_space) is gym.spaces.MultiDiscrete:
+            IPython.embed()
+            pass
+        elif type(action_space) is gym.spaces.MultiBinary:
+            IPython.embed()
+            pass
+        elif type(action_space) is gym.spaces.tuple:
+            # Tuple of different action spaces
+            # https://github.com/openai/gym/blob/master/gym/envs/algorithmic/algorithmic_env.py
+            IPython.embed()
+            pass
+
+        assert hasattr(observation_space, 'shape') and len(observation_space.shape) == 1
+        assert hasattr(action_space, 'shape')
+        self.in_dim = observation_space.shape
+        self.n_in = int(np.prod(observation_space.shape))
+        self.lin1 = nn.Linear(self.n_in, 512)
+        self.relu1 = nn.ReLU()
+        self.lin2 = nn.Linear(512, 1024)
+        self.relu2 = nn.ReLU()
+        self.lin3 = nn.Linear(1024, 1024)
+        self.relu3 = nn.ReLU()
+        self.lin4 = nn.Linear(1024, 512)
+        self.relu4 = nn.ReLU()
+        self.lin5 = nn.Linear(512, 256)
+        self.relu5 = nn.ReLU()
+        self.lin6 = nn.Linear(256, 128)
+        self.relu6 = nn.ReLU()
+        self.lin7 = nn.Linear(128, self.n_out)
 
     def forward(self, x):
-        x = x
-        return None
-
+        x = self.relu1(self.lin1(x))
+        x = self.relu2(self.lin2(x))
+        x = self.relu3(self.lin3(x))
+        x = self.relu4(self.lin4(x))
+        x = self.relu5(self.lin5(x))
+        x = self.relu6(self.lin6(x))
+        x = self.out_activation(self.lin7(x))
+        return x
 
 class MNISTNet(AbstractESModel):
     """ 
@@ -161,18 +302,33 @@ class MNISTNet(AbstractESModel):
     """
     def __init__(self):
         super(MNISTNet, self).__init__()
+        self.in_dim = torch.Size((1, 28, 28))
         self.conv1 = nn.Conv2d(1, 10, kernel_size=(5, 5))
-        self.relu1 = nn.ReLU()
-        #self.conv1_bn = nn.BatchNorm2d(10)
+        self.conv1_bn = nn.BatchNorm2d(10)
+        self.conv1_pool = nn.MaxPool2d(kernel_size=(2,2), stride=None, padding=0, dilation=1, return_indices=False, ceil_mode=False)
+        self.conv1_relu = nn.ReLU()
+
         self.conv2 = nn.Conv2d(10, 20, kernel_size=(5, 5))
-        self.relu2 = nn.ReLU()
-        #self.conv2_bn = nn.BatchNorm2d(20)
-        #self.conv2_drop = nn.Dropout2d()
+        self.conv2_bn = nn.BatchNorm2d(20)
+        self.conv2_pool = nn.MaxPool2d(kernel_size=(2,2), stride=None, padding=0, dilation=1, return_indices=False, ceil_mode=False)
+        self.conv2_relu = nn.ReLU()
+
         self.fc1 = nn.Linear(320, 50)
-        self.relu3 = nn.ReLU()
+        self.fc1_bn = nn.BatchNorm1d(50)
+        self.fc1_relu = nn.ReLU()
+
         self.fc2 = nn.Linear(50, 10)
-        self.lsof = nn.LogSoftmax(dim=1)
+        self.fc2_logsoftmax = nn.LogSoftmax(dim=1)
+
         self._initialize_weigths()
+
+    def forward(self, x):
+        x = self.conv1_relu(self.conv1_pool(self.conv1_bn(self.conv1(x))))
+        x = self.conv2_relu(self.conv2_pool(self.conv2_bn(self.conv2(x))))
+        x = x.view(-1, 320)
+        x = self.fc1_relu(self.fc1_bn(self.fc1(x)))
+        x = self.fc2_logsoftmax(self.fc2(x))
+        return x
 
     def _initialize_weigths(self):
         # gain = nn.init.calculate_gain('relu')
@@ -190,22 +346,35 @@ class MNISTNet(AbstractESModel):
         if self.fc2.bias is not None:
             self.fc2.bias.data.zero_()
 
-    def forward(self, x):
-        x = self.relu1(F.max_pool2d(self.conv1(x), 2))
-        x = self.relu2(F.max_pool2d(self.conv2(x), 2))
-        x = x.view(-1, 320)
-        #x = F.relu(F.dropout(self.fc1(x), training=self.training))
-        x = self.relu3(self.fc1(x))
-        x = self.lsof(self.fc2(x))
 
-        # #x = self.conv1_bn(F.relu(F.max_pool2d(self.conv1(x), 2)))
-        # #x = self.conv2_drop(self.conv2_bn(F.relu(F.max_pool2d(self.conv2(x), 2))))
-        # x = F.relu(F.max_pool2d(self.conv1(x), 2))
-        # x = F.relu(F.max_pool2d(self.conv2(x), 2))
-        # x = x.view(-1, 320)
-        # #x = F.relu(F.dropout(self.fc1(x), training=self.training))
-        # x = F.relu(self.fc1(x))
-        # x = F.log_softmax(self.fc2(x), dim=1)
+class MNISTNetNoBN(MNISTNet):
+    """This version uses no batch normalization
+    """
+    def __init__(self):
+        super(MNISTNetNoBN, self).__init__()
+        self.in_dim = torch.Size((1, 28, 28))
+        self.conv1 = nn.Conv2d(1, 10, kernel_size=(5, 5))
+        self.conv1_pool = nn.MaxPool2d(kernel_size=(2,2), stride=None, padding=0, dilation=1, return_indices=False, ceil_mode=False)
+        self.conv1_relu = nn.ReLU()
+
+        self.conv2 = nn.Conv2d(10, 20, kernel_size=(5, 5))
+        self.conv2_pool = nn.MaxPool2d(kernel_size=(2,2), stride=None, padding=0, dilation=1, return_indices=False, ceil_mode=False)
+        self.conv2_relu = nn.ReLU()
+
+        self.fc1 = nn.Linear(320, 50)
+        self.fc1_relu = nn.ReLU()
+
+        self.fc2 = nn.Linear(50, 10)
+        self.fc2_logsoftmax = nn.LogSoftmax(dim=1)
+
+        self._initialize_weigths()
+
+    def forward(self, x):
+        x = self.conv1_relu(self.conv1_pool(self.conv1(x)))
+        x = self.conv2_relu(self.conv2_pool(self.conv2(x)))
+        x = x.view(-1, 320)
+        x = self.fc1_relu(self.fc1(x))
+        x = self.fc2_logsoftmax(self.fc2(x))
         return x
 
 
