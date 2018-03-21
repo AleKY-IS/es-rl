b83360c89a2e6560b5a630537e03151482373aea
Jakob Havtorn <jakob.havtorn@gmail.com>
2018-03-21 16:36:38 +0100

    Added safety check before running

--- utils/clean_subdirs.py
+++ utils/clean_subdirs.py
@@ -7,6 +7,12 @@ import IPython
 
 
 if __name__ == '__main__':
+    # Ask to continue
+    r = input('This script should not be run while algorithms are executing as this risks deleting their checkpoints.\nProceed? (y/n) ')
+    if r not in ['y', 'Y']:
+        print('Script ended. No files deleted.')
+        exit(0)
+    IPython.embed()
     # Input
     parser = argparse.ArgumentParser(description='Experiments')
     parser.add_argument('-d', type=str, metavar='directory', help='Directory to clean')
@@ -22,9 +28,13 @@ if __name__ == '__main__':
     # Run
     for root, directories, filenames in os.walk(args.d):
         i = 0
-        for filename in filenames: 
+        for filename in filenames:
             if filename in args.f:
                 os.remove(os.path.join(root, filename))
                 i += 1
+        if len(filenames) == 1 and filenames[0] == 'init.log':
+            os.remove(os.path.join(root, filenames[0]))
+            os.rmdir(os.path.join(root))
+            i += 1
         if i > 0:
             print('Removed {:d} {:s} in {:s}'.format(i, 'file' if i == 1 else 'files', root))
