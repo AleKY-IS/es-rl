f6131c66ede89e876da88b820343abd319ac46a0
Jakob Havtorn <jakob.havtorn@gmail.com>
2018-03-26 20:07:43 +0200

    Renamed

--- data-analysis/analyze_E005-E011.py
+++ data-analysis/analyze_E005-E011.py
@@ -33,9 +33,12 @@ parser.add_argument('-d', type=str, default=None, metavar='--directory', help='T
 args = parser.parse_args()
 
 if not args.d:
-    args.d = "/home/jakob/mnt/Documents/es-rl/experiments/checkpoints/E005"
-    # args.d = "/home/jakob/Dropbox/es-rl/experiments/checkpoints/E005-sca"
-    # args.d = "/Users/Jakob/mnt/Documents/es-rl/experiments/checkpoints/E005-sca"
+    # args.d = "/home/jakob/mnt/Documents/es-rl/experiments/checkpoints/E005"   # Linux HPC
+    args.d = "/home/jakob/Dropbox/es-rl/experiments/checkpoints/E005-sca"     # Linux
+    # args.d = "/Users/Jakob/mnt/Documents/es-rl/experiments/checkpoints/E005"  # Mac
+    # args.d = "/home/jakob/mnt/Documents/es-rl/experiments/checkpoints/E011"   # Linux HPC
+    # args.d = "/home/jakob/Dropbox/es-rl/experiments/checkpoints/E011-sca"     # Linux 
+
 save_dir = os.path.join(args.d, 'analysis')
 if not os.path.exists(save_dir):
     os.mkdir(save_dir)
@@ -73,27 +76,44 @@ for s in stats:
     # Add pseudo start time to all times
     abs_walltimes = s['walltimes'] + psuedo_start_time
     # Append pseudo start time to top of series and compute differences
-    s['time_per_generation'] = pd.concat([pd.Series(psuedo_start_time), abs_walltimes]).diff().dropna()
-    s['parallel_fraction'] = s['workertimes']/s['time_per_generation']
+    s['time_per_iteration'] = pd.concat([pd.Series(psuedo_start_time), abs_walltimes]).diff().dropna()
+    s['parallel_fraction'] = s['workertimes']/s['time_per_iteration']
 
 # Compute mean and std over groups
-time_per_generation_means = np.array([])
-time_per_generation_stds = np.array([])
+time_per_iteration_means = np.array([])
+time_per_iteration_stds = np.array([])
 parallel_fraction_means = np.array([])
 parallel_fraction_stds = np.array([])
 associated_bs = np.array([])
+worker_perturbation_pairs = []
 for i in range(0, len(algorithm_states)):
-    time_per_generation_means = np.append(time_per_generation_means, np.mean(stats[i]['time_per_generation']))
-    time_per_generation_stds = np.append(time_per_generation_stds, np.std(stats[i]['time_per_generation']))
-    parallel_fraction_means = np.append(parallel_fraction_means, np.mean(stats[i]['parallel_fraction']))
-    parallel_fraction_stds = np.append(parallel_fraction_stds, np.std(stats[i]['parallel_fraction']))
+    wp_pair = (algorithm_states[i]['workers'], algorithm_states[i]['perturbations'])
+    if wp_pair in worker_perturbation_pairs:
+        idx = worker_perturbation_pairs.index(wp_pair)
+        time_per_iteration_means[idx] = np.min([time_per_iteration_means[idx], np.mean(stats[i]['time_per_iteration'])])
+        time_per_iteration_stds[idx] = np.std(time_per_iteration_means[idx])
+        parallel_fraction_means[idx] = np.min([parallel_fraction_means[idx], np.mean(stats[i]['parallel_fraction'])])
+        parallel_fraction_stds[idx] = np.std(parallel_fraction_means[idx])
+
+        # time_per_iteration_means[idx] = (time_per_iteration_means[idx] + np.mean(stats[i]['time_per_iteration']))/2
+        # time_per_iteration_stds[idx] = (time_per_iteration_stds[idx] + np.std(stats[i]['time_per_iteration']))/2
+        # parallel_fraction_means[idx] = (parallel_fraction_means[idx] + np.mean(stats[i]['parallel_fraction']))/2
+        # parallel_fraction_stds[idx] = (parallel_fraction_stds[idx] + np.std(stats[i]['parallel_fraction']))/2
+    else:
+        worker_perturbation_pairs.append(wp_pair)
+        time_per_iteration_means = np.append(time_per_iteration_means, np.mean(stats[i]['time_per_iteration']))
+        time_per_iteration_stds = np.append(time_per_iteration_stds, np.std(stats[i]['time_per_iteration']))
+        parallel_fraction_means = np.append(parallel_fraction_means, np.mean(stats[i]['parallel_fraction']))
+        parallel_fraction_stds = np.append(parallel_fraction_stds, np.std(stats[i]['parallel_fraction']))
 
 # Sort according to number of workers. This makes lines in the plots nice
+workers = np.array([p[0] for p in worker_perturbation_pairs])
+perturbations = np.array([p[1] for p in worker_perturbation_pairs])
 sort_indices = np.argsort(workers)
 workers = workers[sort_indices]
 perturbations = perturbations[sort_indices]
-time_per_generation_means = time_per_generation_means[sort_indices]
-time_per_generation_stds = time_per_generation_stds[sort_indices]
+time_per_iteration_means = time_per_iteration_means[sort_indices]
+time_per_iteration_stds = time_per_iteration_stds[sort_indices]
 parallel_fraction_means = parallel_fraction_means[sort_indices]
 parallel_fraction_stds = parallel_fraction_stds[sort_indices]
 
@@ -105,14 +125,14 @@ if ONLY_LOGARITHMICALLY_SPACED_WORKER_TIMES:
     keep_ids = np.array(keep_ids)
     workers = workers[keep_ids]
     perturbations = perturbations[keep_ids]
-    time_per_generation_means = time_per_generation_means[keep_ids]
-    time_per_generation_stds = time_per_generation_stds[keep_ids]
+    time_per_iteration_means = time_per_iteration_means[keep_ids]
+    time_per_iteration_stds = time_per_iteration_stds[keep_ids]
     parallel_fraction_means = parallel_fraction_means[keep_ids]
     parallel_fraction_stds = parallel_fraction_stds[keep_ids]
 
 # Confidence intervals
 cis_times = []
-for m, s in zip(time_per_generation_means, time_per_generation_stds):
+for m, s in zip(time_per_iteration_means, time_per_iteration_stds):
     interval = sp.stats.norm.interval(0.95, loc=m, scale=s)
     half_width = (interval[1] - interval[0])/2
     cis_times.append(half_width)
@@ -142,20 +162,21 @@ for i_pert in sorted(np.unique(perturbations_SP), reverse=False):
     SP[i_pert] = np.zeros(len(workers_SP[i_pert]))
     SP_std[i_pert] = np.zeros(len(workers_SP[i_pert]))
     f[i_pert] = {k: np.zeros(len(workers_SP[i_pert])) for k in np.unique(perturbations)}
-    # Indices of the minimal number of workers that did this perturbation
-    ids_minimal_workers_with_i_pert = workers == 1  # workers == workers_SP[i_pert].min()
-    # Index of this job
-    ids_minimal_workers_i_pert = np.logical_and(ids_minimal_workers_with_i_pert, ids_perturbation)
+    # Indices of the 1 worker job for this perturbation
+    ids_minimal_workers_i_pert = np.logical_and(workers == 1, ids_perturbation)
     if not ids_minimal_workers_i_pert.any():
         continue
-    # Time per generation and of this job
-    T1 = time_per_generation_means[ids_minimal_workers_i_pert]
-    T1_std = time_per_generation_stds[ids_minimal_workers_i_pert]
+    # Time per iteration and of this job
+    T1 = time_per_iteration_means[ids_minimal_workers_i_pert]
+    T1_std = time_per_iteration_stds[ids_minimal_workers_i_pert]
     # Compute speed-ups
-    TP[i_pert] = time_per_generation_means[ids_perturbation]
-    TP_std[i_pert] = time_per_generation_stds[ids_perturbation]
+    TP[i_pert] = time_per_iteration_means[ids_perturbation]
+    TP_std[i_pert] = time_per_iteration_stds[ids_perturbation]
     # Speed-up
-    SP[i_pert] = T1/TP[i_pert]
+    try:
+        SP[i_pert] = T1/TP[i_pert]
+    except ValueError:
+        IPython.embed()
     SP_std[i_pert] = np.sqrt((T1_std / T1)**2 + (TP_std[i_pert] / TP[i_pert])**2) * np.abs(SP[i_pert])  # See Taylor: "Error analysis"
     # Parallel fraction
     f[i_pert] = (1 - TP[i_pert]/T1) / (1 - 1/workers_SP[i_pert])
@@ -164,52 +185,51 @@ for i_pert in sorted(np.unique(perturbations_SP), reverse=False):
 
 
 # Plotting
-# Figure 1: Average time per generation vs number of workers, logarithmic y
+# Figure 1: Average time per iteration vs number of workers, logarithmic y
 fig, ax = plt.subplots()
 legend = []
 for i_pert in sorted(np.unique(perturbations), reverse=False):
     ids = np.where(perturbations == i_pert)
     x = workers[ids]
-    y = time_per_generation_means[ids]
-    s = time_per_generation_stds[ids]
+    y = time_per_iteration_means[ids]
+    s = time_per_iteration_stds[ids]
     legend.append(str(i_pert))
     ax.errorbar(x, y, yerr=s, fmt='-o')
 plt.xlabel('Number of workers')
-plt.ylabel('Average time per generation [s]')
+plt.ylabel('Average time per iteration [s]')
 box = ax.get_position()
 ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
 ax.legend(legend, title='Perturbations', loc='center left', bbox_to_anchor=(1, 0.5))
-ax.set_yscale('log')
 plt.savefig(os.path.join(save_dir,'E005-scaling-01.pdf'), bbox_inches='tight')
 
-# Figure 2: Average time per generation vs number of workers, logarithmic y and x
+# Figure 2: Average time per iteration vs number of workers, logarithmic y and x
 fig, ax = plt.subplots()
 legend = []
 for i_pert in sorted(np.unique(perturbations), reverse=False):
     ids = np.where(perturbations == i_pert)
     x = workers[ids]
-    y = time_per_generation_means[ids]
-    s = time_per_generation_stds[ids]
+    y = time_per_iteration_means[ids]
+    s = time_per_iteration_stds[ids]
     legend.append(str(i_pert))
     ax.errorbar(x, y, yerr=s, fmt='-o')
 box = ax.get_position()
 ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
 ax.legend(legend, title='Perturbations', loc='center left', bbox_to_anchor=(1, 0.5))
 plt.xlabel('Number of workers')
-plt.ylabel('Average time per generation [s]')
+plt.ylabel('Average time per iteration [s]')
 ax.set_yscale('log')
 ax.set_xscale('log', basex=2)
 plt.savefig(os.path.join(save_dir,'E005-scaling-02.pdf'), bbox_inches='tight')
 
-# Figure 3: Average time per generation vs number of workers, logarithmic y and x, smoothed interpolation
+# Figure 3: Average time per iteration vs number of workers, logarithmic y and x, smoothed interpolation
 fig, ax = plt.subplots()
 ax.set_prop_cycle(None)
 legend = []
 for i_pert in sorted(np.unique(perturbations), reverse=False):
     ids = np.where(perturbations == i_pert)
     x = workers[ids]
-    y = time_per_generation_means[ids]
-    s = time_per_generation_stds[ids]
+    y = time_per_iteration_means[ids]
+    s = time_per_iteration_stds[ids]
     try:
         tck = sp.interpolate.splrep(x, y, k=3)
     except TypeError:
@@ -221,7 +241,7 @@ for i_pert in sorted(np.unique(perturbations), reverse=False):
     ax.plot(x_interpolated, y_smoothed, color=color)
     ax.errorbar(x, y, yerr=s, fmt='o', color=color)
 plt.xlabel('Number of workers')
-plt.ylabel('Time per generation [s]')
+plt.ylabel('Time per iteration [s]')
 # ax.legend(legend)
 ax.set_yscale('log')
 ax.set_xscale('log', basex=2)
